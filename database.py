82d176c81c1fb46c233f5430527c1f9204a668c3
miguelHenrique390 <miguel.santos27012009@gmail.com>
2026-04-30 16:26:03 -0300

    Add files via upload

--- database.py
+++ database.py
@@ -1,19 +1,34 @@
-import os
 import psycopg2
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
-def get_connection():
-    host = os.environ.get("DB_HOST")
-    print("host------------------: ", host)
-    conn = psycopg2.connect(
-        # host= os.environ.get("DB_HOST"),
-        # database= os.environ.get("DB_NAME"),
-        # user= os.environ.get("DB_USER"),
-        # password= os.environ.get("DB_PASSWORD"),
-        host='localhost',
-        database='catalogo_filmes',
-        user='postgres',
-        password='1234',
-    )
-    return conn
+def get_conection():
+    try:
+        if os.getenv("DB_HOST") == "localhost":
+            conn = psycopg2.connect(
+                host=os.getenv("DB_HOST"),
+                database=os.getenv("DB_NAME"),
+                user=os.getenv("DB_USER"),
+                password=os.getenv("DB_PASSWORD"),
+                sslmode ="disable",
+                channel_binding="disable"
+            )
+
+            return conn
+        else:
+            conn = psycopg2.connect(
+                host=os.getenv("DB_HOST"),
+                database=os.getenv("DB_NAME"),
+                user=os.getenv("DB_USER"),
+                password=os.getenv("DB_PASSWORD"),
+                sslmode=os.getenv("DB_SSLMODE"),
+                channel_binding=os.getenv("DB_CHANNEL_BINDING")
+            )
+            return conn
+    except Exception as ex:
+        print(f'Erro ao conectar ao banco de babos: {ex}')
+        return None
 
